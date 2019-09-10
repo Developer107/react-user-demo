02ecbf97818dd50c60b30245ba2c1ea1928e8f95
Bhumika Soomani <bhumsey24@gmail.com>
2019-09-11 02:37:04 +0530

    chore: Add build folder for netlify CI CD

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ This project was bootstrapped with [create-react-app](https://github.com/faceboo
 #####
 This demo project contains basic user view system where a user can register, login and upload pics in his profile.
 #####
-User can check out the uploaded pics of other users too.
+User can view the uploaded pics of other users too.
 #####
 As this is a demo project just to showcase my front end skills, I haven't used any API but instead used local storage for saving all data.
 ####
---
+++ build/precache-manifest.6ac5c24e0dd4733b087e87251d8fab74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb57d762bbe606213f8e291986c4680",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d25293f82167ef54a0df",
+    "url": "/static/css/main.085f774b.chunk.css"
+  },
+  {
+    "revision": "ad866a2e944bb6db74b2",
+    "url": "/static/js/2.da356c99.chunk.js"
+  },
+  {
+    "revision": "d25293f82167ef54a0df",
+    "url": "/static/js/main.dde437bf.chunk.js"
+  },
+  {
+    "revision": "eef02f71df841792cb87",
+    "url": "/static/js/runtime~main.f9869c2b.js"
+  },
+  {
+    "revision": "3cb22185d54bb23df0762f8cd8c1853e",
+    "url": "/static/media/default.3cb22185.png"
+  }
+]);
